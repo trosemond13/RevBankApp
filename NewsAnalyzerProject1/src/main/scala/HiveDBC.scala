337bc723e11dc53acaa923923c004ef0d2ee47e4
Teddy <trosejr@terpmail.umd.edu>
2021-12-28 18:27:08 -0500

    Added method to supress warnings and a way to get users from table

--- NewsAnalyzerProject1/src/main/scala/HiveDBC.scala
+++ NewsAnalyzerProject1/src/main/scala/HiveDBC.scala
@@ -1,7 +1,15 @@
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, SQLContext, SQLImplicits, SparkSession}
+import com.roundeights.hasher.Implicits._
+import scala.language.postfixOps
 
 object HiveDBC {
+  def suppressLogs(params: List[String]): Unit = {
+    import org.apache.log4j.{Level, Logger}
+    params.foreach(Logger.getLogger(_).setLevel(Level.OFF))
+  }
+
   def connect(): SparkSession = {
+    suppressLogs(List("org", "akka"))
     val spark = SparkSession
       .builder()
       .appName("NewsAnalyzerProject1")
@@ -24,21 +32,29 @@ object HiveDBC {
     spark.sql(query)
   }
 
-  def getUsers(): DataFrame = {
+  def getUsers(): Map[String, (String, Int, Boolean)] = {
     val spark = connect()
+
     executeDML("create table IF NOT EXISTS users(id Int, username String, password String, admin Boolean) row format delimited fields terminated by ','", spark)
     val users = executeQuery("select * from users", spark)
+
     if(users.count()==0) {
-      executeDML("insert into users values (1, 'root', 'password', true)",spark)
+      val password = "password".sha256.hash
+      executeDML("insert into users values (1, 'root', '" + password+ "', true)", spark)
     }
 
-    val result: Map[String, (String, Int, Boolean)] = users.map(record => record.toList.take(5).mkString("_"))
-    users.show()
+    var usersMap: Map[String, (String, Int, Boolean)] = Map[String, (String, Int, Boolean)]()
 
-    println(users.)
+    users.collect().foreach(row => {
+      val id = row.getInt(0)
+      val username = row.getString(1)
+      val password = row.getString(2)
+      val isAdmin = row.getBoolean(3)
+      usersMap += (username -> (password, id, isAdmin))
+    })
 
     disconnect(spark)
-    users
+    usersMap
   }
 
 }
